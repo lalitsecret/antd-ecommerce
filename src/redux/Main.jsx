d185b43717b4d0f1afa46f9c73b032450fe5a7d1
lalitsecret <lalitsecret@gmail.com>
2021-04-18 08:40:17 +0530

    1 file modified

--- src/redux/Main.jsx
+++ src/redux/Main.jsx
@@ -1,7 +1,7 @@
 import reducer from './reducer'
 import App from '../components/App'
 import {Provider}from 'react-redux'
-import {createStore} form 'redux'
+import {createStore} from 'redux'
 import 'antd/dist/antd.css';
 import '../assets/style.css'
 const store=createStore(reducer)
